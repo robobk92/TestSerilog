d73444f3746a704715ce3d863aec9c7796ae1dcc
Long Lương <robobk92@gmail.com>
2025-02-10 15:48:13 +0700

    Make Many Message

--- TestSerilog/Form1.cs
+++ TestSerilog/Form1.cs
@@ -48,6 +48,11 @@ namespace TestSerilog
             Console.WriteLine(k);
             Log.Error("Long Dep trai", "1 khong 2");
             MessageBox.Show("mot ngay moi bat dau!long");
+            MessageBox.Show("mot ngay moi bat dau!long");
+            MessageBox.Show("mot ngay moi bat dau!long");
+            MessageBox.Show("mot ngay moi bat dau!long");
+            MessageBox.Show("mot ngay moi bat dau!long");
+            MessageBox.Show("mot ngay moi bat dau!long");
         }
     }
 }
