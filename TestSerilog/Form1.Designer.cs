31892404d80ff75f9a2b6cee9dc1e1c57d32749b
LongPC <LongPC@DESKTOP-6EQAI1A>
2025-02-10 15:24:04 +0700

    Add project files.

---
+++ TestSerilog/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TestSerilog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel38 = new System.Windows.Forms.Panel();
+            this.tlp_Vision = new System.Windows.Forms.TableLayoutPanel();
+            this.panel38.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 231);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel38
+            // 
+            this.panel38.Controls.Add(this.tlp_Vision);
+            this.panel38.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel38.Location = new System.Drawing.Point(0, 0);
+            this.panel38.Name = "panel38";
+            this.panel38.Size = new System.Drawing.Size(1467, 153);
+            this.panel38.TabIndex = 1;
+            // 
+            // tlp_Vision
+            // 
+            this.tlp_Vision.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(223)))));
+            this.tlp_Vision.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tlp_Vision.ColumnCount = 6;
+            this.tlp_Vision.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_Vision.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_Vision.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_Vision.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_Vision.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_Vision.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
+            this.tlp_Vision.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlp_Vision.Location = new System.Drawing.Point(0, 0);
+            this.tlp_Vision.Name = "tlp_Vision";
+            this.tlp_Vision.RowCount = 2;
+            this.tlp_Vision.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlp_Vision.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlp_Vision.Size = new System.Drawing.Size(1467, 153);
+            this.tlp_Vision.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1467, 831);
+            this.Controls.Add(this.panel38);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel38.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel38;
+        private System.Windows.Forms.TableLayoutPanel tlp_Vision;
+    }
+}
+
---
+++ TestSerilog/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Serilog;
+namespace TestSerilog
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, Label> dictionary = new Dictionary<string, Label>();
+        public enum WarningType
+        {
+            On = 0x21,
+            Off = 0x41,
+            fail = 0xFF
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            Label label1 = new Label() { 
+            Text = "label1",
+            };
+            Label label2 = new Label()
+            {
+                Text = "label2",
+            };
+            dictionary.Add("No0",label1);
+            dictionary.Add("No1", label2);
+
+
+            tlp_Vision.Controls.Add(dictionary["No0"]);
+            tlp_Vision.Controls.Add(dictionary["No1"]);
+
+
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            byte k = Convert.ToByte("0xFF");
+            Console.WriteLine(k);
+            Log.Error("Long Dep trai", "1 khong 2");
+        }
+    }
+}
